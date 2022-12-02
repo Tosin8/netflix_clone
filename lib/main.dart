19604fa426f63b5455f6aeb00d75fea9e909e4c8
Tosin Ezekiel <tosinezekiel8@gmail.com>
2022-12-02 23:46:27 +0100

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -19,7 +19,7 @@ class MyApp extends StatelessWidget {
         primarySwatch: Colors.blue,
         visualDensity: VisualDensity.adaptivePlaformDensity, 
       ),
-      home: const Scaffold(),
+      home: const HomeScreen(),
     );
   }
 }
