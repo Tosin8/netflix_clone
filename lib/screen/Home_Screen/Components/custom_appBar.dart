e3e184c743b2c7de33a43b4a9e2e81500ccc8f16
Tosin Ezekiel <tosinezekiel8@gmail.com>
2022-12-13 23:58:40 +0100

    Creating AppBarButton StatelessWidget

--- lib/screen/Home_Screen/Components/custom_appBar.dart
+++ lib/screen/Home_Screen/Components/custom_appBar.dart
@@ -9,8 +9,30 @@ class CustomAppBar extends StatelessWidget {
       child: SafeArea(
         child: Row(children: [
           Image.asset('assets/images/logo.png'),
+          const SizedBox(width: 12.0),
+          Row(
+            children: const [_AppBarButton()],
+          )
         ]),
       ),
+    );
+  }
+}
+
+class _AppBarButton extends StatelessWidget {
+  const _AppBarButton({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: () => print('TV Shows'),
+      child: const Text('TV Shows',
+          style: TextStyle(
+              color: Colors.white,
+              fontSize: 16.0,
+              fontWeight: FontWeight.bold)),
     );
   }
 }
