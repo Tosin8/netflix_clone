deba1960519754d1cbae4a13a1f3e8bd199e9c7e
Tosin Ezekiel <tosinezekiel8@gmail.com>
2022-12-15 13:48:25 +0100

    Update content_header.dart

--- lib/screen/Home_Screen/Components/content_header.dart
+++ lib/screen/Home_Screen/Components/content_header.dart
@@ -1,1 +1,24 @@
+import 'package:flutter/material.dart';
 
+class ContentHeader extends StatelessWidget {
+  final Content featuredContent;
+  const ContentHeader({
+    required Key key,
+    @required this.featuredContent,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Stack(
+      children: [
+        Container(
+          height: 500.0,
+          decoration: BoxDecoration(
+              image: DecorationImage(
+            image: AssetImage(featuredContent.imageUrl),
+          )),
+        )
+      ],
+    );
+  }
+}
