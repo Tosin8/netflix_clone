76dcd51a86061025a79ed48033046d5a4c5db9e4
Tosin Ezekiel <tosinezekiel8@gmail.com>
2022-12-15 13:15:45 +0100

    Update home_screen.dart

--- lib/screen/Home_Screen/home_screen.dart
+++ lib/screen/Home_Screen/home_screen.dart
@@ -23,6 +23,12 @@ class _HomeScreenState extends State<HomeScreen> {
     super.initState();
   }
 
+  @override 
+  void dispose() {
+_scrollController.dispose()
+super.dispose(); 
+  }
+
   @override
   Widget build(BuildContext context) {
     final Size screenSize = MediaQuery.of(context).size;
