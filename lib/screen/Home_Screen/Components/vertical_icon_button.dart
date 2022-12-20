a26f88981edc9253cec38d346b4043d07a8ab2bd
Tosin Ezekiel <tosinezekiel8@gmail.com>
2022-12-20 23:38:49 +0100

    Update vertical_icon_button.dart

--- lib/screen/Home_Screen/Components/vertical_icon_button.dart
+++ lib/screen/Home_Screen/Components/vertical_icon_button.dart
@@ -1,6 +1,11 @@
 import 'package:flutter/material.dart';
 
 class VerticalIconButton extends StatelessWidget {
+
+  final IconData icon; 
+  final String title; 
+  final Function onTap; 
+  
   @override
   Widget build(BuildContext context) {
     return Container();
