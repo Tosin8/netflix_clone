2e920f4b36a2c30f31f455eb458465f851d01915
Tosin Ezekiel <tosinezekiel8@gmail.com>
2022-12-21 23:44:33 +0100

    Update content_header.dart

--- lib/screen/Home_Screen/Components/content_header.dart
+++ lib/screen/Home_Screen/Components/content_header.dart
@@ -64,6 +64,7 @@ class _PlayButton extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return FlatButton.icon(
+      padding: const EdgeInsets.fromLTRB(15.0, 5.0, 20.0, 5.0), 
       onPressed: () => print('Play'),  
       color: Colors.white, 
       icon: Icon(Icons.play_arrow, size: 30.0), 
@@ -72,7 +73,7 @@ class _PlayButton extends StatelessWidget {
        style: TextStyle(
         fontSize: 16.0, 
         fontWeight: FontWeight.w600, 
-      ))
+      )), 
     );
   }
 }
